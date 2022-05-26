0c23a5824f5e0deff34e36b40a0953f4ea21d87b
Mario Rezende <mariorez@gmail.com>
2022-05-26 12:49:34 -0300

    first commit

---
+++ src/commonMain/kotlin/main.kt
@@ -0,0 +1,112 @@
+import com.soywiz.korge.Korge
+import com.soywiz.korge.view.alignRightToLeftOf
+import com.soywiz.korge.view.alignRightToRightOf
+import com.soywiz.korge.view.alignTopToBottomOf
+import com.soywiz.korge.view.alignTopToTopOf
+import com.soywiz.korge.view.centerOn
+import com.soywiz.korge.view.centerXOn
+import com.soywiz.korge.view.container
+import com.soywiz.korge.view.image
+import com.soywiz.korge.view.position
+import com.soywiz.korge.view.roundRect
+import com.soywiz.korge.view.size
+import com.soywiz.korge.view.text
+import com.soywiz.korim.color.Colors
+import com.soywiz.korim.color.RGBA
+import com.soywiz.korim.font.readBitmapFont
+import com.soywiz.korim.format.readBitmap
+import com.soywiz.korim.text.TextAlignment
+import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.Rectangle
+
+suspend fun main() = Korge(width = 480, height = 640, title = "2048", bgcolor = RGBA(253, 247, 240)) {
+
+    /* ASSETS ********************************************/
+    val font = resourcesVfs["clear_sans.fnt"].readBitmapFont()
+    val restartImg = resourcesVfs["restart.png"].readBitmap()
+    val undoImg = resourcesVfs["undo.png"].readBitmap()
+
+    val cellSize = views.virtualWidth / 5.0
+    val fieldSize = 50 + 4 * cellSize
+    val leftIndent = (views.virtualWidth - fieldSize) / 2
+    val topIndent = 150.0
+
+    val bgField = roundRect(fieldSize, fieldSize, 5.0, fill = Colors["#b9aea0"]) {
+        position(leftIndent, topIndent)
+    }
+
+    for (i in 0..3) {
+        for (j in 0..3) {
+            roundRect(cellSize, cellSize, 5.0, fill = Colors["#cec0b2"]) {
+                x = leftIndent + 10 + (cellSize + 10) * i
+                y = topIndent + 10 + (cellSize + 10) * j
+            }
+        }
+    }
+
+    /* LOGO **********************************************/
+    val bgLogo = roundRect(cellSize, cellSize, 5.0, fill = Colors["#edc403"]) {
+        position(leftIndent, 30.0)
+    }
+
+    text("2048", cellSize * 0.5, Colors.WHITE, font).centerOn(bgLogo)
+
+    /* BEST SCORE ****************************************/
+    val bgBest = roundRect(cellSize * 1.5, cellSize * 0.8, 5.0, fill = Colors["#bbae9e"]) {
+        alignRightToRightOf(bgField)
+        alignTopToTopOf(bgLogo)
+    }
+
+    text("BEST", cellSize * 0.25, RGBA(239, 226, 210), font) {
+        centerXOn(bgBest)
+        alignTopToTopOf(bgBest, 5.0)
+    }
+
+    text("0", cellSize * 0.5, Colors.WHITE, font) {
+        setTextBounds(Rectangle(0.0, 0.0, bgBest.width, cellSize - 24.0))
+        alignment = TextAlignment.MIDDLE_CENTER
+        alignTopToTopOf(bgBest, 12.0)
+        centerXOn(bgBest)
+    }
+
+    /* CURRENT SCORE *************************************/
+    val bgScore = roundRect(cellSize * 1.5, cellSize * 0.8, 5.0, fill = Colors["#bbae9e"]) {
+        alignRightToLeftOf(bgBest, 24)
+        alignTopToTopOf(bgBest)
+    }
+
+    text("SCORE", cellSize * 0.25, RGBA(239, 226, 210), font) {
+        centerXOn(bgScore)
+        alignTopToTopOf(bgScore, 5.0)
+    }
+
+    text("0", cellSize * 0.5, Colors.WHITE, font) {
+        setTextBounds(Rectangle(0.0, 0.0, bgScore.width, cellSize - 24.0))
+        alignment = TextAlignment.MIDDLE_CENTER
+        centerXOn(bgScore)
+        alignTopToTopOf(bgScore, 12.0)
+    }
+
+    /* BUTTONS *******************************************/
+    val btnSize = cellSize * 0.3
+
+    val restartBlock = container {
+        val background = roundRect(btnSize, btnSize, 5.0, fill = RGBA(185, 174, 160))
+        image(restartImg) {
+            size(btnSize * 0.8, btnSize * 0.8)
+            centerOn(background)
+        }
+        alignTopToBottomOf(bgBest, 5)
+        alignRightToRightOf(bgField)
+    }
+
+    val undoBlock = container {
+        val background = roundRect(btnSize, btnSize, 5.0, fill = RGBA(185, 174, 160))
+        image(undoImg) {
+            size(btnSize * 0.6, btnSize * 0.6)
+            centerOn(background)
+        }
+        alignTopToTopOf(restartBlock)
+        alignRightToLeftOf(restartBlock, 5.0)
+    }
+}
